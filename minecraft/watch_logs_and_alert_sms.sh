b49a5571f5b6b6b876c2706f3c7a5e91ac37bf50
SlopSecSquirrel <38402309+SlopSecSquirrel@users.noreply.github.com>
2022-08-05 11:03:58 -0400

    Update watch_logs_and_alert_sms.sh

--- minecraft/watch_logs_and_alert_sms.sh
+++ minecraft/watch_logs_and_alert_sms.sh
@@ -1,3 +1,8 @@
+
+Oliver Brooks <ollie.brooks.ncc@gmail.com>
+11:02 (0 minutes ago)
+to ohexfortyone
+
 #!/bin/bash
 result=$(/usr/sbin/lsof -iTCP:25565 -sTCP:ESTABLISHED | tail -n +2 | wc -l)
 echo -n $result > players.txt
@@ -10,11 +15,12 @@ while [ 1 -gt 0 ]; do
                 #logged in
                 sleep 5 #(give the log a chance to get written to)
                 username=$(grep "logged in" /home/ec2-user/valhesia/logs/latest.log  | tail -n 1 | cut -d: -f4 | cut -d"[" -f1)
-                ip=$(grep "logged in" /home/ec2-user/valhesia/logs/latest.log  | tail -n 1 | cut -d: -f4 | cut -d"[" -f2)
+                ip=$(grep "logged in" /home/ec2-user/valhesia/logs/latest.log  | tail -n 1 | cut -d: -f4 | cut -d"[" -f2 | sed 's/^.//')
 
-                message="$result new players have joined. Username is probably $username, and they connected from $ip"
+                message="$result new players have joined. Username is $username, and they connected from $ip"
                 echo $message
-                aws sns publish --message "$message" --phone-number +1REDACTED
+                #aws sns publish --message "$message" --phone-number +1REDACTED
+                aws sns publish --topic-arn arn:aws:sns:us-east-1:171160792142:minecraft-sms --message "$message"
                 echo -n $result > players.txt
         fi
         if [ $result -lt $previousPlayers ]
@@ -22,9 +28,10 @@ while [ 1 -gt 0 ]; do
                 #lost connection
                 sleep 5 #(give the log a chance to get written to)
                 username=$(grep "lost connection" /home/ec2-user/valhesia/logs/latest.log  | tail -n 1 | cut -d: -f4 | cut -d" " -f2)
-                message="Someone (named '$username') has left the server. There are now $result players playing."
+                message="'$username' has left the server. There are now $result players playing."
                 echo $message
-                aws sns publish --message "$message" --phone-number +1REDACTED
+                #aws sns publish --message "$message" --phone-number +1REDACTED
+                aws sns publish --topic-arn arn:aws:sns:us-east-1:171160792142:minecraft-sms --message "$message"
                 echo -n $result > players.txt
         fi
         sleep 30
